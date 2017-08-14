c66b39b214b797ac987d4fbafbd7dc245f53adce
Yasser Arturo Carreon Lopez <yasser carreon@tiempodev.local>
2017-08-14 11:38:46 -0500

    Change app state from reducers

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,7 +1,7 @@
 export default function () {
   return [
-    { title: 'JS the good parts' },
-    { title: 'Harry Potter' },
-    { title: 'The dark tower' }
+    { title: 'JS the good parts', pages: 101 },
+    { title: 'Harry Potter', pages: 200 },
+    { title: 'The dark tower', pages: 302 }
   ]
 }
